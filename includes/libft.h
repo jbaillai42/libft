a4c241433ddd4cbc113e73eedc9948b1396884e8
jbaillai42 <John42@John.local>
2016-09-20 00:52:15 +0200

    [ADD LIBFT.H]

---
+++ includes/libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jobailla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/18 18:03:08 by jobailla          #+#    #+#             */
+/*   Updated: 2016/09/20 00:48:24 by jobailla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+char	*ft_strcpy(char *dest, char *src);
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	*ft_strstr(char *str, char *to_find);
+char	*ft_strcat(char *dest, char *src);
+char	*ft_strdup(char *src);
+
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strncmp(char *s1, char *s2, unsigned int n);
+int		ft_atoi(char *str);
+
+#endif
