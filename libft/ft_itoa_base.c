b405201f4c9f90e2856be23cd85890c12edc29c7
jbaillai42 <jobailla@student.42.fr>
2018-03-29 01:07:39 +0200

    update itoa_base

--- libft/ft_itoa_base.c
+++ libft/ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: jobailla <jobailla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/11 18:29:24 by jobailla          #+#    #+#             */
-/*   Updated: 2017/12/04 18:09:25 by jobailla         ###   ########.fr       */
+/*   Updated: 2018/03/29 01:07:21 by jobailla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,16 @@ char	*ft_itoa_base(int n, int base)
 	size_t		len;
 	intmax_t	nb;
 
+	nb = n < 0 ? -(long)n : (long)n;
+	len = ft_nbrlen_base(n, base);
+	len += n < 0 ? 1 : 0;
 	if (base < 2)
 		return (0);
-	len = ft_nbrlen_base(n , base);
-	nb = (long)n;
-	if (n < 0)
-	{
-		nb = -nb;
-		len++;
-	}
 	if (!(ret = ft_strnew(len)))
 		return (NULL);
 	while (len--)
 	{
-		ret[len] = (nb % base < 10) ? (char)nb % base + '0' : (char)nb % base + 'A' - 10;
+		ret[len] = (nb % base < 10) ? nb % base + '0' : nb % base + 'A' - 10;
 		nb /= base;
 	}
 	n < 0 ? ret[0] = '-' : 0;
